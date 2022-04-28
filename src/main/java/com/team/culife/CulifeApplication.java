b28d77ec22eacd08a648ca257a57f15a03cf48eb
ImMino <rlaalsgh5151@naver.com>
2022-04-28 11:51:35 +0900

    init project

---
+++ src/main/java/com/team/culife/CulifeApplication.java
@@ -0,0 +1,13 @@
+package com.team.culife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/culife/CulifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.culife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
