236c2fa428fc0ec8997f8fc246e44089f7cdfd5e
jiyeon <jinnyk1842@naver.com>
2022-05-18 09:57:26 +0900

    마무리

--- src/main/java/com/team/culife/controller/WarningController.java
+++ src/main/java/com/team/culife/controller/WarningController.java
@@ -1,13 +1,10 @@
 package com.team.culife.controller;
 
-import java.sql.SQLException;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team.culife.service.WarningService;
