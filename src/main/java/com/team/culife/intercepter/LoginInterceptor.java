7308bc54a695e1c0df980f0a636d5857c89e9a4a
song7351 <dnjs9082@gmail.com>
2022-05-18 16:00:34 +0900

    [FEAT] 인터셉터추가(2)

---
+++ src/main/java/com/team/culife/intercepter/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package com.team.culife.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        
+        int grade = (int)session.getAttribute("grade");
+		System.out.println(grade);
+		if(grade != 0) { //로그인 상태
+			return true;
+		} else { // 로그아웃상태
+			response.sendRedirect(request.getContextPath()+"/");
+			return false;
+		}
+    }
+}
