dd0b20686f523a68b63534bfe9f841ef54789387
ImMino <rlaalsgh5151@naver.com>
2022-05-21 20:13:41 +0900

    [STYLE] 마이페이지 - 알림 기능 .js 파일 추가

---
+++ src/main/webapp/js/mypage/alert.js
@@ -0,0 +1,41 @@
+$(function(){
+	getAlert();
+});
+function getAlert(){
+	var container = $("#mypage_notification_ul");
+	var url = "/mypage/alert"
+	$.ajax({
+		url : url,
+		type: "GET",
+		success:function(data){
+			container.empty();
+			console.log(data.items.length);
+			$("#mypage_notification_count").text("("+data.items.length+")");
+			data.items.forEach(function(element, index){
+				console.log(element);
+				if(index > 0){
+					container.append(`
+					<li><hr class="dropdown-divider"></li>
+					<li style="width:15vw; margin-left:4px; margin-right:4px; font-size:1.4rem;">
+						${element.content}<small style="color:gray"> ${element.create_date} days ago</small>
+					</li>
+					`)
+
+				}
+				else{
+					container.append(`
+					<li style="width:15vw; margin-left:4px; margin-right:4px; font-size:1.4rem;">
+						${element.content} <small style="color:gray"> ${element.create_date} days ago</small>
+					</li>
+					`)
+				}
+			
+			});
+		},error : function(error){
+			console.log(error);
+		}
+	});
+}
+$(document).on("click","#dropdownMenuClickableInside",function(){
+	getAlert();
+});
