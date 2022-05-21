1aa5bc4406bd62f7a2824062f0ea9239f36a25f4
suhyeon1032 <daram_9@naver.com>
2022-05-21 18:24:25 +0900

    Update ExhibitionController.java

--- src/main/java/com/team/culife/controller/ExhibitionController.java
+++ src/main/java/com/team/culife/controller/ExhibitionController.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
